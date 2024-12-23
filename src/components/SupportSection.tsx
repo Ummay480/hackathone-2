e3c707bde5ead52b3d01c3fee7cca29f889e3363
Ummay Kulsoom <141995571+Ummay480@users.noreply.github.com>
2024-12-23 13:35:37 +0500

    Update SupportSection.tsx

--- src/components/SupportSection.tsx
+++ src/components/SupportSection.tsx
@@ -2,7 +2,7 @@ const SupportSection = () => (
     <section className="bg-black text-gray-300 py-12">
       <div className="container mx-auto text-center">
         <h2 className="text-2xl font-bold text-yellow-500 mb-4">Still Need Our Support?</h2>
-        <p className="text-gray-400 mb-6">Don't wait, make a smart & logical quote here. It's pretty easy.</p>
+        <p className="text-gray-400 mb-6">Don&lsquo;t wait&#39; make a smart & logical quote here. It&lsquo;s pretty easy.</p>
         <form className="flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-4">
           <input
             type="email"
