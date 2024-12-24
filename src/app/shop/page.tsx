5506446f9642179f19679c0eda42e1f3a1aa4fa3
Ummay <ummay480@gmail.com>
2024-12-24 11:24:30 +0500

    changes

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,4 +1,3 @@
-import Header from '@/components/Header';
 
 import ProductDetails from '@/components/ProductDetails';
 import SimilarProducts from '@/components/SimilarProducts';
@@ -7,7 +6,7 @@ import ProductGallery from '@/components/ProductGallery';
 
 const Shop = () => (
   <>
-    <Header />
+   
     <main className="container mx-auto px-6">
       <h2 className="text-2xl font-bold my-6">Shop Details</h2>
       
