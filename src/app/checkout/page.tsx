dce246f1c92c92e57f17f0cde455af25e537642c
Ummay Kulsoom <141995571+Ummay480@users.noreply.github.com>
2024-12-29 18:45:39 +0500

    Update page.tsx

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Hero from "../components/HeroBanner";
+import Hero from "@/components/HeroBanner";
 import Image from "next/image";
 
 export default function CheckoutPage() {
