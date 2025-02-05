1e6b2377cbdaa4e7b78b088ee8167f6d5a796d61
Ummay <ummay480@gmail.com>
2025-02-05 14:26:04 +0500

    changes

--- src/app/api/food/route.ts
+++ src/app/api/food/route.ts
@@ -1,9 +1,13 @@
-// import { NextResponse } from 'next/server';
-// import { readFileSync } from 'fs';
-// import { join } from 'path';
+import { NextResponse } from 'next/server'; 
+import { readFileSync } from 'fs'; 
+import { join } from 'path';
 
-// export async function GET() {
-//   const filePath = join(process.cwd(), 'src/data/chef.json');
-//   const data = JSON.parse(readFileSync(filePath, 'utf-8'));
-//   return NextResponse.json(data);
-// }
+export async function GET() {
+  try {
+    const filePath = join(process.cwd(), 'src/data/chef.json');
+    const data = JSON.parse(readFileSync(filePath, 'utf-8'));
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error: 'Failed to read file' }, { status: 500 });
+  }
+}
