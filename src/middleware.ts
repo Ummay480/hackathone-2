833fd1a98f2ea3e815547732622ab1dc09d5acb7
Ummay <ummay480@gmail.com>
2025-02-11 15:52:33 +0500

    changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,8 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 
-export default clerkMiddleware((req: NextRequest) => {
-  return NextResponse.next();
-});
+export default clerkMiddleware();
 
-// Configure which routes the middleware applies to
+// Middleware Configuration
 export const config = {
-  matcher: ["/((?!_next|.*\\..*).*)"], // Apply to all routes except static files (_next, public assets)
+  matcher: ["/((?!_next|.*\\..*).*)"], // Apply to all routes except static files
 };
